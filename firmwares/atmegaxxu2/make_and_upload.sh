a493fe288019df9415ec70127dce7b28f777dc00
Justin Mammarella <jmamma@gmail.com>
2022-05-24 16:00:38 +1000

    fix

--- firmwares/atmegaxxu2/make_and_upload.sh
+++ firmwares/atmegaxxu2/make_and_upload.sh
@@ -3,7 +3,7 @@
 if [ -z $@ ]; then
 rm out.hex
 
-cd LUFA_210130
+cd LUFA_100807
 make clean -j 8
 cd ..
 cd arduino-usbdfu
@@ -26,9 +26,9 @@ AVR_DIR="/Applications/Arduino.app/Contents/Java/hardware/tools/avr"
   cd ..
   ${AVR_DIR}/bin/avr-size arduino-usbdfu/Arduino-usbdfu.hex
   ${AVR_DIR}/bin/avr-size arduino-usbserial/Arduino-usbserial.hex
-  (sed '$d' arduino-usbserial/Arduino-usbserial.hex; cat arduino-usbdfu/Arduino-usbdfu.hex) > out_210130.hex
+  (sed '$d' arduino-usbserial/Arduino-usbserial.hex; cat arduino-usbdfu/Arduino-usbdfu.hex) > out.hex
 fi
-  ${AVR_DIR}/bin/avr-size  out_210130.hex
-avrdude -c usbasp -p m32u2 -U flash:w:out_210130.hex -U lfuse:w:0xFF:m -U hfuse:w:0xD9:m -U efuse:w:0xF4:m -U lock:w:0x0F:m -B10
+  ${AVR_DIR}/bin/avr-size  out.hex
+avrdude -c usbasp -p m32u2 -U flash:w:out.hex -U lfuse:w:0xFF:m -U hfuse:w:0xD9:m -U efuse:w:0xF4:m -U lock:w:0x0F:m -B10
 #avrdude -c usbasp -p m16u2 -U flash:w:out.hex -U lfuse:w:0xFF:m -U hfuse:w:0xD9:m -U efuse:w:0xF4:m -U lock:w:0x0F:m -B10
 
